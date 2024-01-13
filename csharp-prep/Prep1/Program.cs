a4e3eb45cbfe214fb2ef481ffb11863ac6146e9d
Joyce Jensen <Jjoycee@byui.edu>
2024-01-13 15:14:02 -0700

    Changed and completed the C# Prep 1-Variables and Input/Output assignment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // gets the user's first name and saves it in the string: (first_name)
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        // gets the user's last name and saves it in the string: (last_name)
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
