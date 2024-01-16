23cf773d0e0f51576e55db2ce554f2ed80b35b4b
Joyce Jensen <Jjoycee@byui.edu>
2024-01-16 13:33:12 -0700

    Completed Prep3 activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // random number generator
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,101);
+        Console.WriteLine($"What is the magic number? {randomNumber}");
+
+        int userNumber = 0;
+
+        // while user number doesn't equal random number
+        while (randomNumber != userNumber)
+        {
+            Console.Write("What is your guess? ");
+            userNumber = int.Parse(Console.ReadLine());
+            // if the number is higher
+            if (userNumber > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            // if the number is lower
+            else if (userNumber < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            // when they guess the correct number.
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
