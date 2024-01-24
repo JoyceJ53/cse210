66f27b5013044f6497da23ddc5f6da4b17099073
Joyce Jensen <Jjoycee@byui.edu>
2024-01-24 14:45:37 -0700

    Completed the Resume Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Professor";
+        job2._company = "Brigham Young University - Idaho";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        // job1.Display();
+        // job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Joyce Jensen";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
