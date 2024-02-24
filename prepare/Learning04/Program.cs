7cfdc391949d3f6533dc0a53874a5641e3f5b431
Joyce Jensen <Jjoycee@byui.edu>
2024-02-24 13:15:55 -0700

    Completed Learning 04 activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // assignment super class
+        Console.WriteLine();
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        // math sub class
+        Console.WriteLine();
+        MathAssignment assignmentmath = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignmentmath.GetSummary());
+        Console.WriteLine(assignmentmath.GetHomeworkList());
+
+        // writing sub class
+        Console.WriteLine();
+        WritingAssignment assignmentwriting = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignmentwriting.GetSummary());
+        Console.WriteLine(assignmentwriting.GetWritingInformation());
+        Console.WriteLine();
     }
 }
